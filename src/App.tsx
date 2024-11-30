d8bc9e95a6fdfd5c52729e7da393b2da7bb6dfca
Syd Samoyed <syd.lin@outlook.com>
2024-11-30 11:18:15 -0800

    Bootstrap application with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
     <>
-      <h1>Hi! Welcome to the Adrift Engine Client!</h1>
-      <p>
+      <h1 className="text-3xl">Hi! Welcome to the Adrift Engine Client!</h1>
+      <p className="text-base font-bold">
         Eventually, you'll be able to play a text game here. Pretty cool, right?
       </p>
     </>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,tsx}", "index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
