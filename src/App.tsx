2118b0180d162665a06edab3129592afe46fe312
Syd Samoyed <syd.lin@outlook.com>
2024-11-30 10:31:50 -0800

    Set up basic Vite application

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1>Hi! Welcome to the Adrift Engine Client!</h1>
+      <p>
+        Eventually, you'll be able to play a text game here. Pretty cool, right?
+      </p>
+    </>
+  );
+}
+
+export default App;
